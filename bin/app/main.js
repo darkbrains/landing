8757f4a4c2c2d2e2f3f71065c38dd8d49a6b97ef
blackdocs <aram.blackdocs@gmail.com>
2023-10-27 02:02:38 +0400

    changes

---
+++ bin/app/main.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const PORT = 8887;
+const HOST = 'localhost';
+const winston = require('winston');
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+
+app.use((req, res, next) => {
+    if (req.originalUrl.startsWith('/static/')) {
+      next();
+    } else {
+      winston.info({
+        message: 'Request received',
+        method: req.method,
+        url: req.originalUrl,
+        ip: req.ip,
+      });
+      next();
+    }
+});
+
+
+const customFormat = winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message, method, url, ip }) => {
+      return JSON.stringify({ timestamp, level, message, method, url, ip });
+  })
+);
+
+ 
+winston.configure({
+    level: 'debug',
+    format: customFormat,
+    transports: [
+        new winston.transports.Console(),
+    ],
+});
+  
+
+app.use((err, req, res, next) => {
+    winston.error({
+        message: 'Error occurred',
+        error: err.message,
+        stack: err.stack,
+    });
+    if (res.statusCode === 500) {
+        return res.status(500).render('server-error');
+    }
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+
+app.get('/api/healthz', (req, res) => {
+    winston.info('Processing request at /api/healthz');
+    res.render('healthz');
+});
+
+
+app.get('/', (req, res) => {
+  winston.info('Processing request at /');
+  winston.info('Processing redirect to /en');
+  res.render('index');
+});
+
+
+app.use((req, res) => {
+    winston.warn('Request for undefined route', { url: req.originalUrl, ip: req.ip });
+    res.status(404).render('not-found');
+});
+
+
+app.listen(PORT, () => {
+  winston.info(`Server started: http://${HOST}:${PORT}`);
+});
