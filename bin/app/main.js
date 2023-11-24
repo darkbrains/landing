92f46d3b262d77cbeec0534fb616016e8e0656aa
blackdocs <aram.blackdocs@gmail.com>
2023-11-24 17:58:09 +0400

    instrumentation with Prometheus

--- bin/app/main.js
+++ bin/app/main.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = 8887;
+const METRICS_PORT = 5887;
 const HOST = 'localhost';
 const winston = require('winston');
+const client = require('prom-client');
+const metricsApp = express();
+const collectDefaultMetrics = client.collectDefaultMetrics;
 
+collectDefaultMetrics({ timeout: 5000 });
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -66,12 +71,33 @@ app.get('/', (req, res) => {
 });
 
 
+metricsApp.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', client.register.contentType);
+    winston.info('Processing request at /metrics');
+    res.end(await client.register.metrics());
+  } catch (err) {
+    res.status(500).end(err);
+  }
+});
+
+
 app.use((req, res) => {
     winston.warn('Request for undefined route', { url: req.originalUrl, ip: req.ip });
     res.status(404).render('not-found');
 });
 
 
+metricsApp.use((req, res) => {
+  winston.warn('Request for undefined route', { url: req.originalUrl, ip: req.ip });
+  res.status(404).render('not-found');
+});
+
+
 app.listen(PORT, () => {
   winston.info(`Server started: http://${HOST}:${PORT}`);
+});
+
+metricsApp.listen(METRICS_PORT, () => {
+  winston.info(`Metrics server listening on port http://${HOST}:${METRICS_PORT}`);
 });
